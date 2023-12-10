894d14095cb202cd06a2234024ccb65d291da792
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2023-12-10 17:44:26 +0400

    done pretty ui

--- add/main.py
+++ add/main.py
@@ -3,6 +3,7 @@ import typing
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget
 from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
+from PyQt5.QtGui import QMovie
 
 from sidebar import Ui_MainWindow
 
@@ -18,16 +19,28 @@ class MainWindow(QMainWindow):
 
     # Function for searching
     def on_search_btn_clicked(self):
-        print("on search")
-        self.ui.stackedWidget.setCurrentIndex(2)
+        self.ui.stackedWidget.setCurrentIndex(3)
         search_text = self.ui.search_input.text().strip()
+        if search_text == "Home" or search_text == "home":
+            self.ui.stackedWidget.setCurrentIndex(0)
+            return
+        if search_text == "dashboard" or search_text == "Dashboard":
+            self.ui.stackedWidget.setCurrentIndex(1)
+            return
+        if search_text == "user page" or search_text == "user":
+            self.ui.stackedWidget.setCurrentIndex(2)
+            return
+        
         if search_text:
-            self.ui.label_3.setText(search_text)
+            self.movie = QMovie("img/2loading_gif.gif")
+            self.ui.photo_lable.setMovie(self.movie)
+            self.movie.start()
+            self.ui.label_6.setText(f"The page \"{search_text}\" doesn't exist")
 
     ## Function for changing page to user page
     def on_user_btn_clicked(self):
         print("on user")
-        self.ui.stackedWidget.setCurrentIndex(3)
+        self.ui.stackedWidget.setCurrentIndex(2)
 
     ## Change QPushButton Checkable status when stackedWidget index changed
     def on_stackedWidget_currentChanged(self, index):
@@ -45,41 +58,32 @@ class MainWindow(QMainWindow):
             
     ## functions for changing menu page
     def on_home_btn_1_toggled(self):
-        print("on_home_btn_1_toggled")
         self.ui.stackedWidget.setCurrentIndex(0)
     
     def on_home_btn_2_toggled(self):
-        print("on_home_btn_2_toggled")
         self.ui.stackedWidget.setCurrentIndex(0)
 
     def on_dashboard_btn_1_toggled(self):
-        print("on_dashborad_btn_1_toggled")
         self.ui.stackedWidget.setCurrentIndex(1)
 
     def on_dashboard_btn_2_toggled(self):
-        print("on_dashborad_btn_2_toggled")
         self.ui.stackedWidget.setCurrentIndex(1)
-
-
-
-    # def on_customers_btn_2_toggled(self):
-    #     self.ui.stackedWidget.setCurrentIndex(4)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    ## loading style file
-    with open("add/style.qss", "r") as style_file:
-        style_str = style_file.read()
-    app.setStyleSheet(style_str)
+    # ## loading style file
+    # with open("add/style.qss", "r") as style_file:
+    #     style_str = style_file.read()
+    # app.setStyleSheet(style_str)
 
     ## loading style file, Example 2
 
-    # style_file = QFile("style.qss")
-    # style_file.open(QFile.ReadOnly | QFile.Text)
-    # style_stream = QTextStream(style_file)
-    # app.setStyleSheet(style_stream.readAll())
+    style_file = QFile("add/style.qss")
+    style_file.open(QFile.ReadOnly | QFile.Text)
+    style_stream = QTextStream(style_file)
+    app.setStyleSheet(style_stream.readAll())
 
 
     window = MainWindow()
