4db37b2f2188612f207c128edacc60bf5d682036
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2023-12-19 17:28:27 +0400

    upd pass

---
+++ test_git.py
@@ -0,0 +1,24 @@
+# from github import Github
+# import base64
+
+# name = "c0de1sl1fe"
+
+# g = Github()
+
+# user = g.get_user(name)
+# # print(test)
+
+# for content in user.get_repo("auto_saver_app").get_get_contents():
+#     print(content)
+# #     # check if it's a Python file
+#     if content.path.endswith(".txt"):
+#         print(content.decoded_content.decode("utf-8"))
+
+# repo = user.get_repo("auto_saver_app")
+# contents = repo.get_contents("auto_saver_app")
+
+# rawdata = contents.decoded_content
+# decoded = base64.decodebytes(rawdata)
+
+# with open('test_1.txt', 'wb') as f:
+#     f.write(decoded)
