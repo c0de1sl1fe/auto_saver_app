aae61c9dc9f4b1e2a2b086fb26ded89a674d963e
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2023-12-19 18:01:59 +0400

    upd pass

--- test_git.py
+++ test_git.py
@@ -14,15 +14,5 @@ from utils import InterfaceFileOperation
 # #     # check if it's a Python file
 #     if content.path.endswith(".txt"):
 #         print(content.decoded_content.decode("utf-8"))
-a = InterfaceFileOperation()
-print(a.check_user("", "test"))
-
-
-# repo = user.get_repo("auto_saver_app")
-# contents = repo.get_contents("auto_saver_app")
-
-# rawdata = contents.decoded_content
-# decoded = base64.decodebytes(rawdata)
-
-# with open('test_1.txt', 'wb') as f:
-#     f.write(decoded)
+# a = InterfaceFileOperation()
+# print(a.check_user("", "test"))
